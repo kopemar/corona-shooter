e1d586651a01b66da8257ea487f27a667d2cb5ab
Martina Kopecká <martina.kopecka@juicymo.cz>
2020-03-19 16:46:13 +0100

    Add time indicator

--- game.js
+++ game.js
@@ -1,17 +1,44 @@
 class Game {
+
     constructor() {
-        // const restartButton = document.getElementById("restart");
+        this.score = 300;
+        this.scoreIndicator = document.getElementById("score_indicator");
     }
 
     start() {
         alert("The game has started");
+        this.autorun();
+    }
+
+     autorun() {
+        this.interval = setInterval('automaticallyAdd()', 500);
+     }
+
+    addScore(amount) {
+        this.score += amount;
+        let width = 600 - this.score;
+        this.scoreIndicator.style.width = `${width}px`;
     }
 }
 
-const restartButton = document.getElementById("restart");
+function automaticallyAdd() {
+    // alert("Interval is over");
+    game.addScore(-5);
+    if (game.score > 0) {
 
-function handleEvent() {
-    new Game().start()
+    }
+    else {
+        clearInterval(game.interval)
+    }
+
 }
 
-restartButton.addEventListener('click', handleEvent);
+let game = new Game();
+
+const restartButton = document.getElementById("restart");
+
+restartButton.addEventListener('click', function () {
+    game.start();
+    game.addScore(10)
+});
+
