f5d15a175fc393c6ea3350d94ffde653599062d8
Martina Kopecká <martina.kopecka@juicymo.cz>
2020-03-20 11:01:36 +0100

    Test core of game mechanics

--- game.js
+++ game.js
@@ -1,41 +1,61 @@
+const restartButton = document.getElementById("restart");
+const gameOverBox = document.getElementById("game_over_box");
+const playground = document.getElementById("playground");
+const scoreField = document.getElementById("score");
+
 class Game {
 
     constructor() {
-        this.score = 300;
+        this.score = 0;
+        this.opponent = 300;
         this.scoreIndicator = document.getElementById("score_indicator");
     }
 
     start() {
-        alert("The game has started");
-        this.autorun();
+        this.run();
+        this.showNewVirus();
     }
 
-     autorun() {
+    run() {
         this.interval = setInterval('automaticallyAdd()', 500);
-     }
+    }
+
+    showNewVirus() {
+        const virus = document.createElement('div');
+        virus.classList.add("virus");
+        playground.appendChild(virus);
+        virus.addEventListener('click', function () {
+            game.addScore(20)
+        })
+
+    }
 
     addScore(amount) {
         this.score += amount;
-        let width = 600 - this.score;
-        this.scoreIndicator.style.width = `${width}px`;
+        this.opponent -= amount / 2;
+        scoreField.innerText = this.score;
+        this.scoreIndicator.style.width = `${this.opponent}px`;
+        if (this.opponent <= 0) {
+            clearInterval(this.interval);
+            alert("You won!");
+        }
     }
 }
 
 function automaticallyAdd() {
     // alert("Interval is over");
     game.addScore(-5);
-    if (game.score > 0) {
+    if (game.opponent <= 600) {
 
     }
     else {
+        gameOverBox.style.visibility = 'visible';
         clearInterval(game.interval)
     }
 
 }
 
 let game = new Game();
-
-const restartButton = document.getElementById("restart");
 
 restartButton.addEventListener('click', function () {
     game.start();
